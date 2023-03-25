77947376f7d972c772cca64b274b734cca249152
Gabriel Terwesten <gabriel@terwesten.net>
2023-03-25 23:38:45 +0100

    Add checks to reading MessageBuffer

--- native/src/MessageBuffer.cpp
+++ native/src/MessageBuffer.cpp
@@ -2,6 +2,9 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <sstream>
+#include <string>
+#include <iostream>
 
 namespace couchbase::dart
 {
@@ -13,22 +16,24 @@ std::string Slice::toString() const
 
 MessageBuffer::MessageBuffer(size_t initialCapacity)
 {
-    _data = (uint8_t *)malloc(initialCapacity);
-    if (_data == nullptr) {
+    _start = (uint8_t *)malloc(initialCapacity);
+    if (_start == nullptr) {
         throw std::bad_alloc();
     }
-    _position = _data;
+    _end = _start;
+    _position = _start;
     _capacity = initialCapacity;
 }
 
 MessageBuffer::~MessageBuffer()
 {
-    free(_data);
+    free(_start);
 }
 
 void MessageBuffer::reset()
 {
-    _position = _data;
+    _end = _position;
+    _position = _start;
 }
 
 void MessageBuffer::writeBool(bool value)
@@ -129,6 +134,7 @@ void MessageBuffer::writeString(const std::string &value)
 
 bool MessageBuffer::readBool()
 {
+    checkCanRead(sizeof(bool));
     bool value = *((bool *)_position);
     _position += sizeof(bool);
     return value;
@@ -136,6 +142,7 @@ bool MessageBuffer::readBool()
 
 uint8_t MessageBuffer::readUInt8()
 {
+    checkCanRead(sizeof(uint8_t));
     uint8_t value = *((uint8_t *)_position);
     _position += sizeof(uint8_t);
     return value;
@@ -143,6 +150,7 @@ uint8_t MessageBuffer::readUInt8()
 
 int8_t MessageBuffer::readInt8()
 {
+    checkCanRead(sizeof(int8_t));
     int8_t value = *((int8_t *)_position);
     _position += sizeof(int8_t);
     return value;
@@ -150,6 +158,7 @@ int8_t MessageBuffer::readInt8()
 
 uint16_t MessageBuffer::readUInt16()
 {
+    checkCanRead(sizeof(uint16_t));
     uint16_t value = *((uint16_t *)_position);
     _position += sizeof(uint16_t);
     return value;
@@ -157,6 +166,7 @@ uint16_t MessageBuffer::readUInt16()
 
 int16_t MessageBuffer::readInt16()
 {
+    checkCanRead(sizeof(int16_t));
     int16_t value = *((int16_t *)_position);
     _position += sizeof(int16_t);
     return value;
@@ -164,6 +174,7 @@ int16_t MessageBuffer::readInt16()
 
 uint32_t MessageBuffer::readUInt32()
 {
+    checkCanRead(sizeof(uint32_t));
     uint32_t value = *((uint32_t *)_position);
     _position += sizeof(uint32_t);
     return value;
@@ -171,6 +182,7 @@ uint32_t MessageBuffer::readUInt32()
 
 int32_t MessageBuffer::readInt32()
 {
+    checkCanRead(sizeof(int32_t));
     int32_t value = *((int32_t *)_position);
     _position += sizeof(int32_t);
     return value;
@@ -178,6 +190,7 @@ int32_t MessageBuffer::readInt32()
 
 uint64_t MessageBuffer::readUInt64()
 {
+    checkCanRead(sizeof(uint64_t));
     uint64_t value = *((uint64_t *)_position);
     _position += sizeof(uint64_t);
     return value;
@@ -185,6 +198,7 @@ uint64_t MessageBuffer::readUInt64()
 
 int64_t MessageBuffer::readInt64()
 {
+    checkCanRead(sizeof(int64_t));
     int64_t value = *((int64_t *)_position);
     _position += sizeof(int64_t);
     return value;
@@ -192,6 +206,7 @@ int64_t MessageBuffer::readInt64()
 
 float MessageBuffer::readFloat()
 {
+    checkCanRead(sizeof(float));
     float value = *((float *)_position);
     _position += sizeof(float);
     return value;
@@ -199,6 +214,7 @@ float MessageBuffer::readFloat()
 
 double MessageBuffer::readDouble()
 {
+    checkCanRead(sizeof(double));
     double value = *((double *)_position);
     _position += sizeof(double);
     return value;
@@ -207,6 +223,7 @@ double MessageBuffer::readDouble()
 Slice MessageBuffer::readData()
 {
     uint64_t length = readUInt64();
+    checkCanRead(length);
     Slice slice = {_position, length};
     _position += length;
     return slice;
@@ -217,14 +234,19 @@ std::string MessageBuffer::readString()
     return readData().toString();
 }
 
-size_t MessageBuffer::length() const
+size_t MessageBuffer::size() const
 {
-    return _position - _data;
+    return _end - _start;
+}
+
+size_t MessageBuffer::offset() const
+{
+    return _position - _start;
 }
 
 size_t MessageBuffer::remainingCapacity() const
 {
-    return _capacity - length();
+    return _capacity - offset();
 }
 
 void MessageBuffer::ensureCapacity(size_t additionalCapacity)
@@ -233,21 +255,33 @@ void MessageBuffer::ensureCapacity(size_t additionalCapacity)
         return;
     }
 
-    size_t oldLength = length();
+    size_t oldLength = offset();
     size_t newLength = oldLength + additionalCapacity;
     size_t newCapacity = _capacity * 2;
     if (newCapacity < newLength) {
         newCapacity = newLength * 2;
     }
 
-    auto newData = (uint8_t *)realloc(_data, newCapacity);
-    if (newData == nullptr) {
+    auto newStart = (uint8_t *)realloc(_start, newCapacity);
+    if (newStart == nullptr) {
         throw std::bad_alloc();
     }
 
-    _data = newData;
-    _position = _data + oldLength;
+    _start = newStart;
+    _position = _start + oldLength;
     _capacity = newCapacity;
+}
+
+void MessageBuffer::checkCanRead(size_t bytesToRead) const
+{
+    if (offset() + bytesToRead > size()) {
+        std::ostringstream oss;
+        oss << "Cannot read " << bytesToRead << " bytes from MessageBuffer of size "
+            << size() << " at offset " << offset() << ".";
+        std::string message = oss.str();
+        std::cerr << message << std::endl;
+        throw std::out_of_range(message);
+    }
 }
 
 }; // namespace couchbase::dart
