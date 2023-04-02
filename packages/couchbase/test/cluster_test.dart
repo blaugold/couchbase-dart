ab79df0d45a2b0d08ad249604a6defe6591d0ef6
Gabriel Terwesten <gabriel@terwesten.net>
2023-04-02 19:46:22 +0200

    Add tests for `Cluster` operations

--- packages/couchbase/test/cluster_test.dart
+++ packages/couchbase/test/cluster_test.dart
@@ -1,134 +1,89 @@
 import 'package:couchbase/couchbase.dart';
 import 'package:test/test.dart';
 
+import 'utils/test_cluster.dart';
+
 void main() async {
-  late Cluster cluster;
+  group('connect', () {
+    group('authentication', () {
+      test('PasswordAuthenticator', () async {
+        await connectToTestCluster(
+          clusterOptions: ClusterOptions(
+            authenticator: testClusterAuthenticator,
+          ),
+        );
+      });
 
-  setUp(() async {
-    cluster = await Cluster.connect(
-      'couchbase://localhost',
-      ClusterOptions(
-        username: 'admin',
-        password: 'password',
-      ),
-    );
+      test('username and password in ClusterOptions', () async {
+        await connectToTestCluster(
+          clusterOptions: ClusterOptions(
+            username: testClusterAuthenticator.username,
+            password: testClusterAuthenticator.password,
+          ),
+        );
+      });
+    });
   });
 
-  tearDown(() async {
-    await cluster.close();
+  group('diagnostics', () {
+    test('default options', () async {
+      final cluster = await connectToTestCluster();
+      final result = await cluster.diagnostics();
+      expect(result.version, 2);
+      expect(result.sdk, startsWith('cxx'));
+      final services = result.services;
+      expect(services.keys, [ServiceType.keyValue]);
+    });
+
+    test('with reportId', () async {
+      final cluster = await connectToTestCluster();
+      final result =
+          await cluster.diagnostics(const DiagnosticsOptions(reportId: 'test'));
+      expect(result.id, 'test');
+    });
   });
 
-  test('diagnostics', () async {
-    final result =
-        await cluster.diagnostics(const DiagnosticsOptions(reportId: 'test'));
-    expect(result.id, 'test');
-    expect(result.version, 2);
-    expect(result.sdk, startsWith('cxx'));
-    final services = result.services;
-    expect(services.keys, containsAll([ServiceType.keyValue]));
-  });
+  group('ping', () {
+    test('default options', () async {
+      final cluster = await connectToTestCluster();
+      final result = await cluster.ping();
+      expect(result.version, 2);
+      expect(result.sdk, startsWith('cxx'));
+      final services = result.services;
+      expect(
+        services.keys,
+        [
+          ServiceType.keyValue,
+          ServiceType.views,
+          ServiceType.management,
+        ],
+      );
+    });
 
-  test('ping', () async {
-    final result = await cluster.ping(
-      const PingOptions(
-        reportId: 'test',
-        bucket: 'test',
-        serviceTypes: [ServiceType.keyValue],
-      ),
-    );
-    expect(result.id, 'test');
-    expect(result.version, 2);
-    expect(result.sdk, startsWith('cxx'));
-    final services = result.services;
-    expect(services.keys, containsAll([ServiceType.keyValue]));
-    final bucketEndpoint = services[ServiceType.keyValue]!.first;
-    expect(bucketEndpoint.bucket, 'test');
-  });
+    test('with bucket', () async {
+      final cluster = await connectToTestCluster();
+      final result = await cluster.ping(const PingOptions(bucket: 'test'));
+      final services = result.services;
+      expect(services.keys, [ServiceType.keyValue]);
+      final bucketEndpoint = services[ServiceType.keyValue]!.single;
+      expect(bucketEndpoint.bucket, 'test');
+    });
 
-  test('insert and get document', () async {
-    final bucket = cluster.bucket('test');
-    final collection = bucket.defaultCollection;
-    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
-    final testDocumentValue = {'hello': 'world'};
-    await collection.insert(testDocumentId, testDocumentValue);
-    final getResult = await collection.get(
-      testDocumentId,
-      const GetOptions(withExpiry: true),
-    );
-    expect(getResult.content, testDocumentValue);
-  });
+    test('with reportId', () async {
+      final cluster = await connectToTestCluster();
+      final result = await cluster.ping(const PingOptions(reportId: 'test'));
+      expect(result.id, 'test');
+    });
 
-  test('sub document lookup: exists', () async {
-    final bucket = cluster.bucket('test');
-    final collection = bucket.defaultCollection;
-    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
-    final testDocumentValue = {'hello': 'world'};
-    await collection.insert(testDocumentId, testDocumentValue);
-    var result = await collection
-        .lookupIn(testDocumentId, [LookupInSpec.exists('hello')]);
-    expect(result.content.single.value, true);
-    result =
-        await collection.lookupIn(testDocumentId, [LookupInSpec.exists('foo')]);
-    expect(result.content.single.value, false);
-  });
-
-  test('fetch all LookupInMacros for document', () async {
-    final bucket = cluster.bucket('test');
-    final collection = bucket.defaultCollection;
-    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
-    final testDocumentValue = {'hello': 'world'};
-    await collection.insert(testDocumentId, testDocumentValue);
-    final result = await collection.lookupIn(
-      testDocumentId,
-      [
-        LookupInSpec.get(LookupInMacro.document),
-        LookupInSpec.get(LookupInMacro.expiry),
-        LookupInSpec.get(LookupInMacro.cas),
-        LookupInSpec.get(LookupInMacro.seqNo),
-        LookupInSpec.get(LookupInMacro.lastModified),
-        LookupInSpec.get(LookupInMacro.isDeleted),
-        LookupInSpec.get(LookupInMacro.valueSizeBytes),
-        LookupInSpec.get(LookupInMacro.revId),
-      ],
-    );
-    expect(result.content.length, 8);
-    expect(result.content[0].error, isNull);
-    expect(
-      (result.content[0].value! as Map<String, Object?>)['datatype'],
-      ['json'],
-    );
-    expect(result.content[1].error, isNull);
-    expect(result.content[1].value, isNull);
-    expect(result.content[2].error, isNull);
-    expect(result.content[2].value, result.cas);
-    expect(result.content[3].error, isNull);
-    expect(result.content[3].value, startsWith('0x'));
-    // TODO: Try to insert document with higher durability, once implemented.
-    // This might allow us to always get a last modified date.
-    // Currently, retrieving the last modified date fails in CI. It seems to
-    // have something to do with how new the cluster/bucket is. Locally, the
-    // last modified date is always returned, after a few mutation operations.
-    // expect(result.content[4].error, isNull);
-    // expect(result.content[4].value, isA<DateTime>());
-    expect(result.content[5].error, isNull);
-    expect(result.content[5].value, false);
-    expect(result.content[6].error, isNull);
-    expect(result.content[6].value, 17);
-    expect(result.content[7].error, isNull);
-    expect(result.content[7].value, '1');
-  });
-
-  test('check if document exists', () async {
-    final bucket = cluster.bucket('test');
-    final collection = bucket.defaultCollection;
-    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
-    final testDocumentValue = {'hello': 'world'};
-
-    var result = await collection.exists(testDocumentId);
-    expect(result.exists, false);
-
-    await collection.insert(testDocumentId, testDocumentValue);
-    result = await collection.exists(testDocumentId);
-    expect(result.exists, true);
+    test('with serviceTypes', () async {
+      final cluster = await connectToTestCluster();
+      final result = await cluster.ping(
+        const PingOptions(
+          serviceTypes: [ServiceType.keyValue],
+        ),
+      );
+      final services = result.services;
+      expect(services.keys, [ServiceType.keyValue]);
+    });
   });
 }
---
+++ packages/couchbase/test/collection_test.dart
@@ -0,0 +1,97 @@
+import 'package:couchbase/couchbase.dart';
+import 'package:test/test.dart';
+
+import 'utils/cluster.dart';
+
+void main() {
+  test('insert and get document', () async {
+    final cluster = await connectToTestCluster();
+    final bucket = cluster.bucket('test');
+    final collection = bucket.defaultCollection;
+    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
+    final testDocumentValue = {'hello': 'world'};
+    await collection.insert(testDocumentId, testDocumentValue);
+    final getResult = await collection.get(
+      testDocumentId,
+      const GetOptions(withExpiry: true),
+    );
+    expect(getResult.content, testDocumentValue);
+  });
+
+  test('sub document lookup: exists', () async {
+    final cluster = await connectToTestCluster();
+    final bucket = cluster.bucket('test');
+    final collection = bucket.defaultCollection;
+    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
+    final testDocumentValue = {'hello': 'world'};
+    await collection.insert(testDocumentId, testDocumentValue);
+    var result = await collection
+        .lookupIn(testDocumentId, [LookupInSpec.exists('hello')]);
+    expect(result.content.single.value, true);
+    result =
+        await collection.lookupIn(testDocumentId, [LookupInSpec.exists('foo')]);
+    expect(result.content.single.value, false);
+  });
+
+  test('fetch all LookupInMacros for document', () async {
+    final cluster = await connectToTestCluster();
+    final bucket = cluster.bucket('test');
+    final collection = bucket.defaultCollection;
+    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
+    final testDocumentValue = {'hello': 'world'};
+    await collection.insert(testDocumentId, testDocumentValue);
+    final result = await collection.lookupIn(
+      testDocumentId,
+      [
+        LookupInSpec.get(LookupInMacro.document),
+        LookupInSpec.get(LookupInMacro.expiry),
+        LookupInSpec.get(LookupInMacro.cas),
+        LookupInSpec.get(LookupInMacro.seqNo),
+        LookupInSpec.get(LookupInMacro.lastModified),
+        LookupInSpec.get(LookupInMacro.isDeleted),
+        LookupInSpec.get(LookupInMacro.valueSizeBytes),
+        LookupInSpec.get(LookupInMacro.revId),
+      ],
+    );
+    expect(result.content.length, 8);
+    expect(result.content[0].error, isNull);
+    expect(
+      (result.content[0].value! as Map<String, Object?>)['datatype'],
+      ['json'],
+    );
+    expect(result.content[1].error, isNull);
+    expect(result.content[1].value, isNull);
+    expect(result.content[2].error, isNull);
+    expect(result.content[2].value, result.cas);
+    expect(result.content[3].error, isNull);
+    expect(result.content[3].value, startsWith('0x'));
+    // TODO: Try to insert document with higher durability, once implemented.
+    // This might allow us to always get a last modified date.
+    // Currently, retrieving the last modified date fails in CI. It seems to
+    // have something to do with how new the cluster/bucket is. Locally, the
+    // last modified date is always returned, after a few mutation operations.
+    // expect(result.content[4].error, isNull);
+    // expect(result.content[4].value, isA<DateTime>());
+    expect(result.content[5].error, isNull);
+    expect(result.content[5].value, false);
+    expect(result.content[6].error, isNull);
+    expect(result.content[6].value, 17);
+    expect(result.content[7].error, isNull);
+    expect(result.content[7].value, '1');
+  });
+
+  test('check if document exists', () async {
+    final cluster = await connectToTestCluster();
+    final bucket = cluster.bucket('test');
+    final collection = bucket.defaultCollection;
+    final testDocumentId = 'test-${DateTime.now().microsecondsSinceEpoch}';
+    final testDocumentValue = {'hello': 'world'};
+
+    var result = await collection.exists(testDocumentId);
+    expect(result.exists, false);
+
+    await collection.insert(testDocumentId, testDocumentValue);
+    result = await collection.exists(testDocumentId);
+    expect(result.exists, true);
+  });
+}
