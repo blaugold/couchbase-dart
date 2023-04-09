fad8a35585eb5ecba817f5ff3151c6228af0ba0c
Gabriel Terwesten <gabriel@terwesten.net>
2023-04-09 19:50:23 +0200

    Add workflow for building release binaries (#26)

--- native/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# Run CMake configure
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
-
-# Run CMake build
-cmake --build build -j $(node -e "console.log(require('os').cpus().length)")
--- native/build_openssl.dart
+++ native/build_openssl.dart
@@ -1,17 +1,23 @@
-import 'dart:convert';
 import 'dart:io';
 
-const compiler = 'ccache cc';
+import 'build_utils.dart';
+
 final openSslSourceDirectory = '${Directory.current.path}/vendor/openssl';
 final openSslBuildDirectory = '${Directory.current.path}/build/vendor/openssl';
 final openSslInstallDirectory = '$openSslBuildDirectory/install';
 
 void main(List<String> arguments) async {
   final String arch;
-  if (arguments.isNotEmpty) {
-    arch = arguments.first;
+  final archIndex = arguments.indexOf('--arch');
+  if (archIndex != -1) {
+    arch = arguments[archIndex + 1];
   } else {
     arch = currentArch();
+  }
+
+  var compiler = 'cc';
+  if (!arguments.contains('--no-ccache')) {
+    compiler = 'ccache $compiler';
   }
 
   await buildOpenSsl(
@@ -27,7 +33,7 @@ String resolveConfiguration({required String arch}) {
   if (Platform.isLinux) {
     if (arch == 'x86_64') {
       return 'linux-x86_64';
-    } else if (arch == 'arm64') {
+    } else if (arch == 'aarch64') {
       return 'linux-aarch64';
     }
   } else if (Platform.isMacOS) {
@@ -60,6 +66,7 @@ Future<void> buildOpenSsl({
       if (Platform.isWindows) configureScript,
       '--prefix=$installDirectory',
       '--openssldir=$installDirectory/ssl',
+      '--libdir=lib',
       'no-ssl3',
       'no-ssl3-method',
       'no-zlib',
@@ -85,77 +92,4 @@ Future<void> buildOpenSsl({
     ['install_sw'],
     workingDirectory: buildDirectory,
   );
-}
-
-Future<void> ensureEmptyDirectory(String path) async {
-  final directory = Directory(path);
-  if (directory.existsSync()) {
-    await directory.delete(recursive: true);
-  }
-  await directory.create(recursive: true);
-}
-
-Future<void> command(
-  String command,
-  List<String> arguments, {
-  String? workingDirectory,
-  Map<String, String>? environment,
-}) async {
-  final formattedCommand = formatCommand(command, arguments);
-  // ignore: avoid_print
-  print('Running $formattedCommand');
-
-  final process = await Process.start(
-    command,
-    arguments,
-    runInShell: Platform.isWindows,
-    mode: ProcessStartMode.inheritStdio,
-    workingDirectory: workingDirectory,
-    environment: environment,
-  );
-  final exitCode = await process.exitCode;
-  if (exitCode != 0) {
-    throw Exception(
-      'Failed to run command (exit code $exitCode): $formattedCommand',
-    );
-  }
-}
-
-String commandForResult(
-  String command,
-  List<String> arguments, {
-  String? workingDirectory,
-  Map<String, String>? environment,
-}) {
-  final result = Process.runSync(
-    command,
-    arguments,
-    runInShell: Platform.isWindows,
-    workingDirectory: workingDirectory,
-    environment: environment,
-    stdoutEncoding: utf8,
-    stderrEncoding: utf8,
-  );
-  final exitCode = result.exitCode;
-  if (exitCode != 0) {
-    throw Exception(
-      'Failed to run (exit code $exitCode) '
-      '${formatCommand(command, arguments)}:'
-      '\n${result.stdout}\n${result.stderr}',
-    );
-  }
-  return result.stdout.toString();
-}
-
-String formatCommand(String command, List<String> arguments) =>
-    [command, ...arguments.map(quoteArgument)].join(' ');
-
-String quoteArgument(String argument) => '"${argument.replaceAll('"', r'\"')}"';
-
-String currentArch() {
-  if (Platform.isLinux || Platform.isMacOS) {
-    return commandForResult('uname', ['-m']).trim().toLowerCase();
-  } else {
-    throw Exception('Unsupported platform: ${Platform.operatingSystem}');
-  }
 }
